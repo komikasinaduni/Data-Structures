bdaa99f4d22852ce2f0c9699b070ad117ec10df2
komikasinaduni <kkomalini@gmail.com>
2025-08-17 01:15:20 -0500

    challenge

--- DS0/src/DS0_CH2.java
+++ DS0/src/DS0_CH2.java
@@ -1,28 +1,33 @@
 public class DS0_CH2 {
-    public static void commonNeighbors (int[] list){
+    public static void commonNeighbors(int[] list) {
         int[] hi = new int[list.length];
-        for(int i = 0; i<list.length; i++){
-            hi[i]=list[i];
-        }
-        for(int i = 0; i<list.length; i++){
-            if(i==0){
-                if(list[i+1]!=list[i])
-                    hi[i]=0;
-                    System.out.println("line 11");
-            }
-            else if(i!=list.length-1 && (list[i-1] != list[i] || list[i+1] != list[i])){
-                hi[i] = 0;
-                System.out.println("line 15");
-            }
-            else if(i==list.length-1){
-                if(list[i-1]!=list[i]){
-                    hi[i]=0;
-                    System.out.println("line 20");
+        if (list.length == 1){
+            list[0] = 0;
+        } else{
+            for (int i = 0; i<list.length; i++) {
+                if (i == 0) {
+                    if (list[i] == list[i+1]) {
+                        hi[i] = list[i];
+                    } else {
+                        hi[i] = 0;
+                    }
+                } else if (i == list.length-1) {
+                    if (list[i] == list[i-1]) {
+                        hi[i] = list[i];
+                    } else {
+                        hi[i] = 0;
+                    }
+                } else {
+                    if (list[i] == list[i-1] || list[i] == list[i+1]) {
+                        hi[i] = list[i];
+                    } else {
+                        hi[i] = 0;
+                    }
                 }
             }
-        }
-        for(int i = 0; i<hi.length; i++){
-            list[i]=hi[i];
+            for (int i = 0; i<list.length; i++) {
+                list[i] = hi[i];
+            }
         }
     }
 }
