e84a5250b4d0892b10c00148b958b2c876fdc7b5
komikasinaduni <kkomalini@gmail.com>
2025-08-18 10:25:12 -0500

    add challenge

--- DS0/src/DS0_CH2.java
+++ DS0/src/DS0_CH2.java
@@ -4,29 +4,26 @@ public class DS0_CH2 {
         for(int i = 0; i<list.length; i++){
             hi[i]=list[i];
         }
-        for(int i = 0; i<list.length; i++){
-            if(i==0){
-                if(list[i+1]!=list[i])
-                    hi[i]=0;
-                    System.out.println("line 11");
-            }
-            else if((list[i-1] == list[i])){
-                i+=0;
-                System.out.println("line 15");
-            } else if(list[i+1] == list[i]){
-                i+=0;
-            } else if(list[i-1] != list[i] && list[i+1] != list[i]){
-                hi[i] = 0;
-            }
-            else if(i==list.length-1){
-                if(list[i-1]!=list[i]){
-                    hi[i]=0;
-                    System.out.println("line 20");
+        if(list.length==1){
+            list[0]=0;
+        } else{
+            for(int i = 0; i<list.length; i++){
+                if(i==0){
+                    if(list[i+1]!=list[i])
+                        hi[i]=0;
+                }
+                else if((list[i-1] != list[i] && i-1 > 0) || (list[i-1] != list[i] && i + 1 < list.length)) {
+                    hi[i] = 0;
+                }else if(i==list.length-1){
+                    if(list[i-1]!=list[i]){
+                        hi[i]=0;
+                        System.out.println("line 20");
+                    }
                 }
             }
-        }
-        for(int i = 0; i<hi.length; i++){
-            list[i]=hi[i];
+            for(int i = 0; i<hi.length; i++){
+                list[i]=hi[i];
+            }
         }
     }
 }
