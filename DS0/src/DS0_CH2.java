f7366b4ab7d69173dbf599f284de6ea6fe70d07a
komikasinaduni <kkomalini@gmail.com>
2025-08-18 20:36:16 -0500

    add challenge

--- DS0/src/DS0_CH2.java
+++ DS0/src/DS0_CH2.java
@@ -1,28 +1,34 @@
 public class DS0_CH2 {
-    public static void commonNeighbors (int[] list){
+    public static void commonNeighbors(int[] list) {
         int[] hi = new int[list.length];
-        for(int i = 0; i<list.length; i++){
-            hi[i]=list[i];
-        }
-        if(list.length==1){
-            list[0]=0;
+        if (list.length == 1){
+            list[0] = 0;
         } else{
-            for(int i = 0; i<list.length; i++){
-                if(i==0){
-                    if(list[i+1]!=list[i])
-                        hi[i]=0;
-                }
-                else if((list[i-1] != list[i] && i-1 > 0) || (list[i-1] != list[i] && i + 1 < list.length)) {
-                    hi[i] = 0;
-                }else if(i==list.length-1){
-                    if(list[i-1]!=list[i]){
-                        hi[i]=0;
-                        System.out.println("line 20");
+            for (int i = 0; i<list.length; i++) {
+                if (i == 0) {
+                    if (list[i] == list[i+1]) {
+                        hi[i] = list[i];
+                        System.out.println("Line 11");
+                    } else {
+                        hi[i] = 0;
+                    }
+                } else if (i == list.length-1) {
+                    if (list[i] == list[i-1]) {
+                        hi[i] = list[i];
+                    } else {
+                        hi[i] = 0;
+                    }
+                } else {
+                    if (list[i] == list[i-1] || list[i] == list[i+1]) {
+                        hi[i] = list[i];
+                        System.out.println("Line 24");
+                    } else {
+                        hi[i] = 0;
                     }
                 }
             }
-            for(int i = 0; i<hi.length; i++){
-                list[i]=hi[i];
+            for (int i = 0; i<list.length; i++) {
+                list[i] = hi[i];
             }
         }
     }
--- DS0/src/DS0_CH3.java
+++ DS0/src/DS0_CH3.java
@@ -1,17 +1,61 @@
 public class DS0_CH3 {
-    /*public static int[] fewest(int cents){
+    public static int[] fewest(int cents){
         int[] hi = new int[10];
-        if(cents%10<5){
-            hi[1] = cents%10;
-        } else{
-            hi[1] = cents%10-5;
+        if (cents%10<5) {
+            hi[0] = cents%5;
+            hi[1] = (cents%10)/5;
+            System.out.println("Line 7");
+        } else {
+            hi[0] = cents%5;
+            hi[1] = 0;
             hi[2] = 1;
+            System.out.println("Line 12");
         }
-
-        if(cents%100<10){
-            cents%100
-        } else{
-            hi[1
+        if (cents>=10000) {
+            hi[9] = cents/10000;
+            cents -= hi[9]*10000;
         }
-    }*/
+        if (cents>=5000) {
+            hi[8] = cents/5000;
+            cents -= hi[8]*5000;
+        }
+        if (cents>=2000) {
+            hi[7] = cents/2000;
+            cents -= hi[7]*2000;
+        }
+        if (cents>=1000) {
+            hi[6] = cents/1000;
+            cents -= hi[6]*1000;
+        }
+        if (cents>=500) {
+            hi[5] = cents/500;
+            cents -= hi[5]*500;
+        }
+        if (cents>=100) {
+            hi[4] = cents/100;
+            cents -= hi[4]*100;
+        }
+        if (cents>=25) {
+            hi[3] = cents/25;
+            cents -= hi[3]*25;
+        }
+        int hello = cents;
+        if (hello>=20) {
+            hi[2] = 2;
+            hello -= 20;
+        } else if (hello>=10) {
+            hi[2] = 1;
+            hello -= 10;
+        } else {
+            hi[2] = 0;
+        }
+        if (hello>=5) {
+            hi[1] = 1;
+            hello -= 5;
+        } else {
+            hi[1] = 0;
+        }
+        hi[0] = hello;
+        return hi;
+    }
 }
