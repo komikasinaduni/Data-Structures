6e2392783ea87ce785a8451efb9299305a47cb0f
komikasinaduni <kkomalini@gmail.com>
2025-08-20 06:56:16 -0500

    Update DS0_CH4.java

--- DS0/src/DS0_CH4.java
+++ DS0/src/DS0_CH4.java
@@ -1,7 +1,7 @@
 public class DS0_CH4 {
     public static boolean balanced(int[][] grid){
-        int left = 0;
-        int right = 0;
+        int[] left = new int[6];
+        int[] right = new int[6];
         boolean x = false;
         for(int i = 0; i<grid.length; i++){
             for(int j = 0; j<grid[0].length; j++){
