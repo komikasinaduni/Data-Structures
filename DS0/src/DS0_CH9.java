30b6be4c68529b29cf8a2c0132c656e37ac556b0
komikasinaduni <kkomalini@gmail.com>
2025-08-22 09:55:26 -0500

    h

---
+++ DS0/src/DS0_CH9.java
@@ -0,0 +1,11 @@
+public class DS0_CH9 {
+    public static int summation(int A, int B){
+        if(A==B){
+            return A;
+        } else{
+            for(int i = A; i<=B; i++){
+
+            }
+        }
+    }
+}
