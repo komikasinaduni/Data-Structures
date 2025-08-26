6acf0644a884803cb608307813ffd3d8c3b7f541
komikasinaduni <kkomalini@gmail.com>
2025-08-26 09:56:52 -0500

    hi

--- DS0/src/DS0_CH12.java
+++ DS0/src/DS0_CH12.java
@@ -1,5 +1,25 @@
 public class DS0_CH12 {
     public static void floodFill(char[][] grid, int col, int row, char toReplace, char newValue){
-        
+        /*
+        ['x','i','x'],
+        ['x','x','i'],
+        ['i','x','i']
+
+        ['c','i','x'],
+        ['c','c','i'],
+        ['i','c','i']
+         */
+        if(row<grid.length-1 && col<grid[0].length-1  && row>0 && col>0 && grid[row][col]==newValue) {
+            floodFill(grid, col + 1, row + 1, toReplace, newValue);
+        }
+        if(row<grid.length-1 && col< grid[0].length-1  && row>0 && col>0 && grid[row+1][col-1]==newValue){
+            floodFill(grid, col-1, row+1, toReplace, newValue);
+        }
+        if (row<grid.length-1 && col<grid[0].length-1  && row>1 && col>1 && grid[row-1][col+1]==newValue){
+            floodFill(grid, col+1, row-1, toReplace, newValue);
+        }
+        if (row<grid.length-1 && col< grid[0].length-1  && row>0 && col>0 && grid[row-1][col-1]==newValue){
+            floodFill(grid, col-1, row-1, toReplace, newValue);
+        }
     }
 }
