583005349092e680bad92ea5216f1068a9b1b280
komikasinaduni <kkomalini@gmail.com>
2025-08-27 09:55:10 -0500

    yo

--- DS0/src/DS0_CH12.java
+++ DS0/src/DS0_CH12.java
@@ -9,16 +9,23 @@ public class DS0_CH12 {
         ['c','c','i'],
         ['i','c','i']
          */
-        if(row<grid.length-1 && col<grid[0].length-1  && row>0 && col>0 && grid[row][col]==newValue) {
+        if(row>0 && col>0 && row< grid.length &&col<grid[0].length){
+            return;
+        }
+        if(row<grid.length-1 && col<grid[0].length-1  && row>0 && col>0 && grid[row][col]==toReplace) {
+            grid[row][col] = newValue;
             floodFill(grid, col + 1, row + 1, toReplace, newValue);
         }
-        if(row<grid.length-1 && col< grid[0].length-1  && row>0 && col>0 && grid[row+1][col-1]==newValue){
+        if(row<grid.length-1 && col< grid[0].length-1  && row>0 && col>0 && grid[row+1][col-1]==toReplace){
+            grid[row+1][col-1] = newValue;
             floodFill(grid, col-1, row+1, toReplace, newValue);
         }
-        if (row<grid.length-1 && col<grid[0].length-1  && row>1 && col>1 && grid[row-1][col+1]==newValue){
+        if (row<grid.length-1 && col<grid[0].length-1  && row>1 && col>1 && grid[row-1][col+1]==toReplace){
+            grid[row-1][col+1] = newValue;
             floodFill(grid, col+1, row-1, toReplace, newValue);
         }
-        if (row<grid.length-1 && col< grid[0].length-1  && row>0 && col>0 && grid[row-1][col-1]==newValue){
+        if (row<grid.length-1 && col< grid[0].length-1  && row>0 && col>0 && grid[row-1][col-1]==toReplace){
+            grid[row-1][col-1] = newValue;
             floodFill(grid, col-1, row-1, toReplace, newValue);
         }
     }
