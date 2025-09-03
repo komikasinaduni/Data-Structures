ee9bcb2a20d81aa1a5cfefebbdb6d8cde6751f12
komikasinaduni <kkomalini@gmail.com>
2025-09-03 09:55:21 -0500

    hi

--- DS1/src/DS1_CH1.java
+++ DS1/src/DS1_CH1.java
@@ -1,5 +1,27 @@
+/* sumLines- The method will receive a file name. The file will contain an unknown number of lines and each line will contain an unknown number of int values, seperated by commans. The method will find the sum of each line and add each sum to an int array. After processing the file, the method should return resulting array. Assume that the received file exisits and is in the correct format.
+ *@param (String) fileName - the name of the file
+ *@return (int[]) - the int array containing the line sums.
+ */
+
+/*Example File:
+numFile3.txt
+3,4,7,8,-9
+        -14,6
+        7
+
+Example Call:
+sumLines("numFile3.txt") -> [13, -8, 7]*/
+
+import java.util.*;
 public class DS1_CH1 {
     public static int[] sumLines(String fileName){
+        ArrayList<Integer> hi = new ArrayList<>();
+        try{
+            Scanner fromFile = new Scanner(fileName);
 
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
