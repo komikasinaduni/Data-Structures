659cebc7ce38f74eaffe95f2862acffdeb2437b3
komikasinaduni <kkomalini@gmail.com>
2025-09-10 23:36:58 -0500

    hi

--- DS1/src/AccessedOn.java
+++ DS1/src/AccessedOn.java
@@ -15,9 +15,13 @@ public class AccessedOn {
             // Everything below this and above the next comment was searched up
             LocalDateTime hey = LocalDateTime.now();
             DateTimeFormatter herro = DateTimeFormatter.ofPattern("E MMM dd HH:mm:ss ");
-            DateTimeFormatter yo = DateTimeFormatter.ofPattern("yyyy");
+            DateTimeFormatter yo = DateTimeFormatter.ofPattern(" yyyy");
+            Calendar calendar = new GregorianCalendar();
+            TimeZone timeZone = calendar.getTimeZone();
+            DateTimeFormatter zoneAbbreviationFormatter
+                    = DateTimeFormatter.ofPattern("zzz", Locale.ENGLISH);
             //-----------------------------------------------------------------------------
-            pw.println(hey.format(herro)+"CDT "+hey.format(yo));
+            pw.println(hey.format(herro) + ZonedDateTime.now(timeZone.toZoneId()).format(zoneAbbreviationFormatter) + hey.format(yo));
             fw.close();
             pw.close();
         } catch (Exception e) {
