e264ba809e719982ca3446b220a98ab7fc9c6120
komikasinaduni <kkomalini@gmail.com>
2025-09-15 09:55:37 -0500

    herro

--- DS1/src/TicTacToe.java
+++ DS1/src/TicTacToe.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class TicTacToe {
     public static String board(char[][] wow){
-        String wow1 = (wow[0][0]+"  |" + wow[0][1]+"  |"+wow[0][2]+"  \n"+"___________\n"+wow[1][0]+"  |" + wow[1][1]+"  |"+wow[1][2]+"  \n"+"___________\n"+wow[2][0]+"  |" + wow[2][1]+"  |"+wow[2][2]+"  ");
+        String wow1 = (" "+wow[0][0]+" | " + wow[0][1]+" | "+wow[0][2]+" \n"+"___________\n "+wow[1][0]+" | " + wow[1][1]+" |"+wow[1][2]+"  \n"+"___________\n "+wow[2][0]+" | " + wow[2][1]+" | "+wow[2][2]+" ");
         return wow1;
     }
     public static void main(String[] args){
@@ -18,7 +18,7 @@ public class TicTacToe {
             herro = yo.nextInt();
             System.out.println("Enter a row from 0 to 2:");
             wassup = yo.nextInt();
-            if(herro!=3&&wassup!=3){
+            if(herro!=3&&wassup!=3&&wow[wassup][herro]!='X'||wow[wassup][herro]!='O'){
                 wow[wassup][herro] = 'X';
                 int yay = (int)(Math.random()*3);
                 int yoy = (int)(Math.random()*3);
@@ -27,6 +27,39 @@ public class TicTacToe {
                     yoy = (int)(Math.random()*3);
                 }
                 wow[yay][yoy]='O';
+                for(int i = 1; i<wow.length-1; i++){
+                    for(int j = 1; j<wow[0].length-1; j++){
+                        if(((wow[1][1]==wow[2][2]&&wow[1][1]==wow[0][0])||(wow[1][1]==wow[2][0]&&wow[1][1]==wow[0][2]))&&!(wow[1][1]==' ')){
+                            if(wow[i][j]=='X'){
+                                System.out.println("X wins");
+                                break;
+                            }
+                            if(wow[i][j]=='O'){
+                                System.out.println("O wins");
+                                break;
+                            }
+                        }
+                        else if((wow[i][j]==wow[i][j-1]&&wow[i][j]==wow[i][j+1])&&!(wow[i][j]==' ')){
+                            if(wow[i][j]=='X'){
+                                System.out.println("X wins");
+                                break;
+                            }
+                            if(wow[i][j]=='O'){
+                                System.out.println("O wins");
+                                break;
+                            }
+                        } else if(wow[i][j]==wow[i-1][j]&&wow[i][j]==wow[i+1][j]&&!(wow[i][j]==' ')){
+                            if(wow[i][j]=='X'){
+                                System.out.println("X wins");
+                                break;
+                            }
+                            if(wow[i][j]=='O'){
+                                System.out.println("O wins");
+                                break;
+                            }
+                        }
+                    }
+                }
             } else{
                 System.out.println("Save complete");
                 System.out.println("Good bye");
