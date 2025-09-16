22165c9af01d51cd46a2e9ab32a98f05191c2ec7
komikasinaduni <kkomalini@gmail.com>
2025-09-15 22:20:10 -0500

    herro

--- DS1/src/TicTacToe.java
+++ DS1/src/TicTacToe.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class TicTacToe {
     public static String board(char[][] wow){
-        String wow1 = (" "+wow[0][0]+" | " + wow[0][1]+" | "+wow[0][2]+" \n"+"___________\n "+wow[1][0]+" | " + wow[1][1]+" |"+wow[1][2]+"  \n"+"___________\n "+wow[2][0]+" | " + wow[2][1]+" | "+wow[2][2]+" ");
+        String wow1 = (" "+wow[0][0]+" | " + wow[0][1]+" | "+wow[0][2]+" \n"+"___________\n "+wow[1][0]+" | " + wow[1][1]+" | "+wow[1][2]+"  \n"+"___________\n "+wow[2][0]+" | " + wow[2][1]+" | "+wow[2][2]+" ");
         return wow1;
     }
     public static void main(String[] args){
@@ -10,7 +10,38 @@ public class TicTacToe {
         int herro = 0;
         int wassup = 0;
         char[][] wow = {{' ', ' ', ' '},{' ', ' ', ' '}, {' ', ' ', ' '}};
-        String yiyi = null;
+        boolean win = false;
+        try {
+            File hi = new File("TicTacToe.txt");
+            if(hi.exists()) {
+                Scanner fr = new Scanner(hi);
+                int r = 0;
+                boolean loaded = false;
+                while (fr.hasNextLine()&&r<3) {
+                    String line = fr.nextLine();
+                    if (line.contains("|")) {
+                        String[] parts = line.split("\\|");
+                        for (int c = 0; c<3; c++) {
+                            char ko = ' ';
+                            if(parts[c].indexOf('X')!=-1){
+                                ko = 'X';
+                            } else if(parts[c].indexOf('O')!=-1){
+                                ko = 'O';
+                            }
+                            wow[r][c] = ko;
+                        }
+                        r++;
+                        loaded=true;
+                    }
+                }
+                if(loaded) {
+                    System.out.println("Game save loaded");
+                }
+                fr.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         while(herro!=3||wassup!=3){
             System.out.println(board(wow));
             System.out.println("Entering a three for the column or row will save the game.");
@@ -18,62 +49,51 @@ public class TicTacToe {
             herro = yo.nextInt();
             System.out.println("Enter a row from 0 to 2:");
             wassup = yo.nextInt();
-            if(herro!=3&&wassup!=3&&wow[wassup][herro]!='X'||wow[wassup][herro]!='O'){
+            if(herro!=3&&wassup!=3&&wow[wassup][herro]==' ') {
                 wow[wassup][herro] = 'X';
-                int yay = (int)(Math.random()*3);
-                int yoy = (int)(Math.random()*3);
-                while(wow[yay][yoy]!=' '){
-                    yay = (int)(Math.random()*3);
-                    yoy = (int)(Math.random()*3);
+                int yay = (int) (Math.random() * 3);
+                int yoy = (int) (Math.random() * 3);
+                while (wow[yay][yoy] != ' ') {
+                    yay = (int) (Math.random() * 3);
+                    yoy = (int) (Math.random() * 3);
                 }
-                wow[yay][yoy]='O';
-                for(int i = 1; i<wow.length-1; i++){
-                    for(int j = 1; j<wow[0].length-1; j++){
-                        if(((wow[1][1]==wow[2][2]&&wow[1][1]==wow[0][0])||(wow[1][1]==wow[2][0]&&wow[1][1]==wow[0][2]))&&!(wow[1][1]==' ')){
-                            if(wow[i][j]=='X'){
-                                System.out.println("X wins");
-                                break;
-                            }
-                            if(wow[i][j]=='O'){
-                                System.out.println("O wins");
-                                break;
-                            }
-                        }
-                        else if((wow[i][j]==wow[i][j-1]&&wow[i][j]==wow[i][j+1])&&!(wow[i][j]==' ')){
-                            if(wow[i][j]=='X'){
-                                System.out.println("X wins");
-                                break;
-                            }
-                            if(wow[i][j]=='O'){
-                                System.out.println("O wins");
-                                break;
-                            }
-                        } else if(wow[i][j]==wow[i-1][j]&&wow[i][j]==wow[i+1][j]&&!(wow[i][j]==' ')){
-                            if(wow[i][j]=='X'){
-                                System.out.println("X wins");
-                                break;
-                            }
-                            if(wow[i][j]=='O'){
-                                System.out.println("O wins");
-                                break;
-                            }
-                        }
-                    }
-                }
-            } else{
+                wow[yay][yoy] = 'O';
+            } else if(herro != 3 && wassup != 3){
+                System.out.println("That spot is already taken. Try again.");
+                continue;
+            }
+            else{
                 System.out.println("Save complete");
                 System.out.println("Good bye");
-                yiyi = board(wow);
                 break;
             }
+            for(int i = 0; i < 3; i++){
+                if(wow[i][0]!= ' '&& wow[i][0]==wow[i][1]&&wow[i][1]==wow[i][2]){
+                    System.out.println(wow[i][0]+" Wins.");
+                    win = true;
+                }
+                if(wow[0][i]!=' '&&wow[0][i]==wow[1][i]&&wow[1][i]==wow[2][i]){
+                    System.out.println(wow[0][i] + " Wins.");
+                    win = true;
+                }
+            }
+            if(wow[0][0]!=' '&&wow[0][0]==wow[1][1]&&wow[1][1]==wow[2][2]){
+                System.out.println(wow[0][0] + " Wins.");
+                win = true;
+            }
+            if(wow[0][2]!=' '&&wow[0][2]==wow[1][1]&&wow[1][1]==wow[2][0]){
+                System.out.println(wow[0][2] + " Wins.");
+                win = true;
+            }
+            if(win) break;
         }
         try{
             File hi = new File("TicTacToe.txt");
-            if(!hi.exists())
-                hi.createNewFile();
             FileWriter fw = new FileWriter(hi, false);
             PrintWriter pw = new PrintWriter(fw);
-            pw.println(yiyi);
+            if(!win){
+                pw.println(board(wow));
+            }
             fw.close();
             pw.close();
         }
