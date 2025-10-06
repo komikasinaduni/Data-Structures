c8f218d52ce1857fa0676a601efb9c284f516a2a
komikasinaduni <kkomalini@gmail.com>
2025-10-06 09:55:10 -0500

    yo

--- DS3/src/DS3_Map.java
+++ DS3/src/DS3_Map.java
@@ -1,47 +1,56 @@
 import java.util.ArrayList;
-
-import java.util.Iterator;
-
 public class DS3_Map <K, V> implements MapInterface<K, V>{
-    private ArrayList<MapEnt> data;
+    private ArrayList<MapEnt<K, V>> data;
     public DS3_Map(){
         data = new ArrayList<>();
-
     }
     public void clear() {
-
+        data.clear();
     }
-
     public boolean containsKey(K key) {
+        for(int i = data.size()-1; i>=0; i++){
+            if(data.get(i).getKey()==key){
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean containsValue(V value) {
+        for(int i = data.size()-1; i>=0; i++){
+            if(data.get(i).getValue()==value){
+                return true;
+            }
+        }
         return false;
     }
 
-    public DS3_Set<MapEnt<K, V>> entrySet() {
-        return null;
+    public DS3_Set<MapEnt<K, V>> entrySet(){
+        DS3_Set<MapEnt<K, V>> yo = new DS3_Set<>();
+        for(int i = data.size()-1; i>=0; i++){
+            yo.add(data.get(i));
+        }
+        return yo;
     }
 
     public V get(K o) {
         for(int i = data.size()-1; i>=0; i++){
             if(o==data.get(i).getKey()){
-                return (V) data.get(i).getValue();
+                return data.get(i).getValue();
             }
         }
         return null;
     }
 
     public boolean isEmpty() {
-        return false;
+        return data.isEmpty();
     }
 
     public V put(K key, V value) {
         for(int i = data.size()-1; i>=0; i++){
-            if(data.get(i).getValue()!=value){
-                V yo = (V) data.get(i).getValue();
-                data.add(new MapEnt(key, value));
+            if(get(key)==get(data.get(i).getKey())){
+                V yo = data.get(i).getValue();
+                data.get(i).setValue(value);
                 return yo;
             }
         }
@@ -50,7 +59,7 @@ public class DS3_Map <K, V> implements MapInterface<K, V>{
     }
 
     public int size() {
-        return 0;
+        return data.size();
     }
 
     public DS3_Set<K> keySet() {
@@ -60,7 +69,7 @@ public class DS3_Map <K, V> implements MapInterface<K, V>{
                 if(data.get(i).getKey()==data.get(j).getKey()){
                     j= data.size()-1;
                 } else{
-                    yo2.add((K) data.get(i).getKey());
+                    yo2.add(data.get(i).getKey());
                 }
             }
         }
@@ -70,7 +79,7 @@ public class DS3_Map <K, V> implements MapInterface<K, V>{
     public ArrayList<V> values() {
         ArrayList<V> yo = new ArrayList<>();
         for(int i = data.size()-1; i>=0; i++) {
-            yo.add(get((K) data.get(i).getKey()));
+            yo.add(get(data.get(i).getKey()));
         }
         return yo;
     }
