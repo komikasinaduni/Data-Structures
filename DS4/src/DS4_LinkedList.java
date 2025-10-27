a2cddab017e6daaa44b22b6ba840c9974c962d83
komikasinaduni <kkomalini@gmail.com>
2025-10-27 09:55:12 -0500

    dfs

--- DS4/src/DS4_LinkedList.java
+++ DS4/src/DS4_LinkedList.java
@@ -57,56 +57,60 @@ public class DS4_LinkedList <E> implements DS4_LinkedList_Interface<E> {
 
     @Override
     public E removeLast() {
-        E last = data2.get(data2.size()-1).getData();
-        data2.remove(data2.size()-1).getData();
-        return last;
+        DS4_LinkedList_Node<E> yo = data2.getNext();
+        boolean x =true;
+        while(data2!=null) {
+            yo = data2.getNext();
+            if (yo== null) {
+                break;
+            } else {
+                data2 = data2.getNext();
+            }
+        }
+        data2.setData(null);
+        return yo.getData();
     }
 
     @Override
     public void addFirst(E data) {
-        data2.add(0, new DS4_LinkedList_Node<>(data));
+
     }
 
     @Override
     public void addLast(E data) {
-        data2.add(data2.size()-1, new DS4_LinkedList_Node<>(data));
 
     }
 
     public void clear() {
-        data2.clear();
+
     }
 
     public int size() {
-        return data2.size();
+
     }
 
     @Override
     public E get(int x) {
-        return data2.get(x).getData();
+
     }
 
     @Override
     public void add(int x, E data) {
-        data2.add(x, new DS4_LinkedList_Node<>(data));
+
     }
 
     @Override
     public E remove(int x) {
-        E remove = data2.get(x).getData();
-        data2.remove(x);
-        return remove;
+
     }
 
     @Override
     public E set(int x, E data) {
-        E set = data2.get(x).getData();
-        data2.set(x, new DS4_LinkedList_Node<>(data));
-        return set;
+
     }
 
     @Override
     public boolean isEmpty() {
-        return data2.isEmpty();
+
     }
 }
