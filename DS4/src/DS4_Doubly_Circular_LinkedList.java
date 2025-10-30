6149f0b4be30a12c96123cea247579c87cf579dc
komikasinaduni <kkomalini@gmail.com>
2025-10-30 09:56:47 -0500

    asdads

---
+++ DS4/src/DS4_Doubly_Circular_LinkedList.java
@@ -0,0 +1,86 @@
+public class DS4_Doubly_Circular_LinkedList<E> implements DS4_Doubly_Circular_LinkedList_Interface<E> {
+
+    public DS4_Doubly_Circular_LinkedList(){
+
+    }
+
+    @Override
+    public DS4_Doubly_Circular_LinkedList_Node<E> getFirstNode() {
+        return null;
+    }
+
+    @Override
+    public DS4_Doubly_Circular_LinkedList_Node<E> getLastNode() {
+        return null;
+    }
+
+    @Override
+    public E getFirst() {
+        return null;
+    }
+
+    @Override
+    public E getLast() {
+        return null;
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public void addFirst(E data) {
+
+    }
+
+    @Override
+    public void addLast(E data) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public E get(int x) {
+        return null;
+    }
+
+    @Override
+    public void add(int x, E data) {
+
+    }
+
+    @Override
+    public E remove(int x) {
+        return null;
+    }
+
+    @Override
+    public E set(int x, E data) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String backwardsToString() {
+        return "";
+    }
+}
