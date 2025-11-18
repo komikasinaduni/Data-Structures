5ab19c055c2c7320a2f6bb8e5cb602fec79f2db0
komikasinaduni <kkomalini@gmail.com>
2025-11-18 09:55:12 -0600

    fdghjkl.

---
+++ DS5/src/DS5_BinarySearchTree.java
@@ -0,0 +1,101 @@
+public class DS5_BinarySearchTree<E extends Comparable> implements DS5_BinarySearchTree_Interface<E> {
+    DS5_BinarySearchTree_Node<E> root;
+
+    public DS5_BinarySearchTree(){
+        root = new DS5_BinarySearchTree_Node<>(null);
+    }
+
+    public DS5_BinarySearchTree(E data){
+        root = new DS5_BinarySearchTree_Node<>(data);
+    }
+    @Override
+    public DS5_BinarySearchTree_Node<E> getRoot() {
+        return root;
+    }
+
+    @Override
+    public String preOrder() {
+        if(root==null)
+            return "[]";
+        return "["+preOrderHelper(root).substring(2)+"]";
+    }
+
+    public String preOrderHelper(DS5_BinarySearchTree_Node<E> temp) {
+        if(temp==null)
+            return "";
+        else
+            return ", "+temp.getData() + preOrderHelper(temp.getLeft()) + preOrderHelper(temp.getRight());
+    }
+
+    @Override
+    public String inOrder() {
+        return "";
+    }
+
+    public String inOrderHelper(DS5_BinarySearchTree_Node<E> temp){
+        if(temp==null)
+            return "";
+        else
+            return ", " + inOrderHelper(temp.getLeft()) + inOrderHelper(temp.getRight()) + temp.getData();
+    }
+
+    @Override
+    public String postOrder() {
+        return "";
+    }
+
+    @Override
+    public E minValue() {
+        return null;
+    }
+
+    @Override
+    public E maxValue() {
+        return null;
+    }
+
+    @Override
+    public int nodeDepth(E value) {
+        return 0;
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+
+    @Override
+    public int maxDepth() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        root = new DS5_BinarySearchTree_Node<>(null);
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(E data) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(E data) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(E data) {
+        return false;
+    }
+}
