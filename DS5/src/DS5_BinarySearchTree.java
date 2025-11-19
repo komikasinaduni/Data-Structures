57864d85b977907dc53e9732c1b91d17b1998129
komikasinaduni <kkomalini@gmail.com>
2025-11-19 09:57:09 -0600

    asdfghjkl;

--- DS5/src/DS5_BinarySearchTree.java
+++ DS5/src/DS5_BinarySearchTree.java
@@ -29,10 +29,26 @@ public class DS5_BinarySearchTree<E extends Comparable> implements DS5_BinarySea
 
     @Override
     public String inOrder() {
-        return "";
+        if(root==null)
+            return "[]";
+        return "["+inOrderHelper(root).substring(2)+"]";
     }
 
     public String inOrderHelper(DS5_BinarySearchTree_Node<E> temp){
+        if(temp==null)
+            return "";
+        else
+            return ", " +  inOrderHelper(temp.getLeft()) + temp.getData() + inOrderHelper(temp.getRight());
+    }
+
+    @Override
+    public String postOrder() {
+        if(root==null)
+            return "[]";
+        return "["+postOrderHelper(root).substring(2)+"]";
+    }
+
+    public String postOrderHelper(DS5_BinarySearchTree_Node<E> temp){
         if(temp==null)
             return "";
         else
@@ -40,28 +56,59 @@ public class DS5_BinarySearchTree<E extends Comparable> implements DS5_BinarySea
     }
 
     @Override
-    public String postOrder() {
-        return "";
-    }
-
-    @Override
     public E minValue() {
-        return null;
+        if(root==null){
+            return null;
+        } else{
+            DS5_BinarySearchTree_Node<E> yo = root;
+            while(yo.getLeft()!=null){
+                yo = yo.getLeft();
+            }
+            return yo.getData();
+        }
     }
 
     @Override
     public E maxValue() {
-        return null;
+        return maxValueHelper(root);
+    }
+
+    public E maxValueHelper(DS5_BinarySearchTree_Node<E> wow){
+        if(wow.getRight()==null){
+            return wow.getData();
+        }
+        return maxValueHelper(wow.getRight());
     }
 
     @Override
     public int nodeDepth(E value) {
-        return 0;
+        return nodeDepthHelper(root, new DS5_BinarySearchTree_Node<E>(value), 0);
+    }
+
+    public int nodeDepthHelper(DS5_BinarySearchTree_Node<E> yo, DS5_BinarySearchTree_Node<E> target, int cd){
+        if(yo==null){
+            return -1;
+        }
+        if(yo==target){
+            return cd;
+        }
+        int ld = nodeDepthHelper(yo.getLeft(), target, cd+1);
+        if(ld!=-1){
+            return ld;
+        }
+        int rd = nodeDepthHelper(yo.getRight(), target, cd+1);
+        return rd;
     }
 
     @Override
     public int height() {
-        return 0;
+        return heightHelper(root);
+    }
+
+    public int heightHelper(DS5_BinarySearchTree_Node<E> yo){
+        int lh = heightHelper(yo.getLeft());
+        int rh = heightHelper(yo.getRight());
+        return 1 + Math.max(lh, rh);
     }
 
     @Override
