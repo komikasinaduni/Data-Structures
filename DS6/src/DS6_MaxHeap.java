596d3181363ad397253ff8d2dc2a346e3cb5397f
komikasinaduni <kkomalini@gmail.com>
2025-12-11 09:45:56 -0600

    qwertyuio

---
+++ DS6/src/DS6_MaxHeap.java
@@ -0,0 +1,26 @@
+public class DS6_MaxHeap<E extends Comparable> implements DS6_HeapInterface<E extends Comparable> {
+    @Override
+    public void insert(E item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
