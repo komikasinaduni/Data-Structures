d9e134d7d1f1904d84c0fff5d016b7283d6965d6
komikasinaduni <kkomalini@gmail.com>
2026-02-27 13:39:35 -0600

    qweshoj

--- DS9/src/FinalCalculator/Main.java
+++ DS9/src/FinalCalculator/Main.java
@@ -5,3 +5,115 @@ public class Main {
         new FinalCalc();
     }
 }
+
+/*
+1
+
+Multiple Choice
+1 point
+
+
+
+Question at position 1
+
+A company wants to automate infrastructure deployment by using infrastructure as code (IaC). The company wants to scale production stacks so the stacks can be deployed in multiple AWS Regions. Which AWS service will meet these requirements?
+
+
+Amazon CloudWatch
+
+
+AWS CloudFormation
+
+
+AWS Config
+
+
+AWS Trusted Advisor
+
+2
+
+Multiple Choice
+1 point
+
+
+
+Question at position 2
+
+Which AWS service is a cloud security posture management (CSPM) service that aggregates alerts from various AWS services and partner products in a standardized format?
+
+
+AWS Trusted Advisor
+
+
+AWS Security Hub
+
+
+Amazon EventBridge
+
+
+Amazon GuardDuty
+
+
+Question at position 4
+4
+
+Multiple Answer
+1 point
+
+
+
+Question at position 4
+
+A company wants to provide managed Windows virtual desktops and applications to its remote employees over secure network connections. Which AWS services can the company use to meet these requirements? (Choose two.)
+
+
+Amazon WorkSpaces
+
+
+Amazon AppStream 2.0
+
+
+Amazon Connect
+
+
+AWS Site-to-Site VPN
+
+
+Amazon Elastic Container Service (Amazon ECS)
+
+
+
+For new AWS customers, what is the EASIEST way to launch a simple WordPress website on AWS?
+
+
+Use the Amazon S3 Web hosting feature.
+
+
+Install WordPress on an Amazon EC2 instance.
+
+
+Run WordPress on an Amazon Lightsail instance.
+
+
+Host the website directly on AWS Cloud Development Kit (AWS CDK).
+
+Which AWS service or feature is used to send both text and email messages from distributed applications?
+
+
+Amazon Simple Queue Service (Amazon SQS)
+
+
+Amazon Simple Email Service (Amazon SES)
+
+
+Amazon Simple Notification Service (Amazon SNS)
+
+
+Amazon CloudWatch alerts
+
+
+
+
+
+
+ */
