457f8229c1e38f53c99f9de554bd0f5a493d0154
komikasinaduni <kkomalini@gmail.com>
2026-04-02 09:55:12 -0500

    dfdfad

--- Key_Mouse/src/LoadNDraw_Frame.java
+++ Key_Mouse/src/LoadNDraw_Frame.java
@@ -7,13 +7,13 @@ import java.io.File;
 
 public class LoadNDraw_Frame extends JFrame {
     private BufferedImage ladder = null;
-    private BufferedImage Floor = null;
+    private BufferedImage floor = null;
 
     public LoadNDraw_Frame(){
         super("Load and draw 2 Images");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         try{
-            Floor = ImageIO.read((new File("Images\\Floor.gif")));
+            floor = ImageIO.read((new File("Images\\Floor.gif")));
             ladder = ImageIO.read((new File("Images\\ladder.gif")));
             System.out.println("All images were loaded Properly.");
         } catch(Exception e){
@@ -30,7 +30,7 @@ public class LoadNDraw_Frame extends JFrame {
         g.fillRect(0,0,getWidth(), getHeight());
 
         g.drawImage(ladder, 50, 50, null);
-        g.drawImage(Floor, 150, 50, null);
+        g.drawImage(floor, 150, 50, null);
     }
 
 }
